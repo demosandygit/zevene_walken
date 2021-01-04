9585c1f662650fed9c0ab6e8240c84f371d96f80
hp <hp@DESKTOP-P577KFP>
2021-01-05 00:30:04 +0530

    First commit

---
+++ zevene_walken/src/test/java/com/ebanking/zevene_walken/Login.java
@@ -0,0 +1,21 @@
+package com.ebanking.zevene_walken;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Login
+{
+	@Test
+	public void login()
+	{
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://localhost/login.do");
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.xpath("//input[@name='pwd']")).sendKeys("manager");
+		driver.findElement(By.id("loginButton")).click();
+		
+	}
+}
